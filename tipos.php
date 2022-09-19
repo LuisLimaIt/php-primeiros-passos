77570a2cca40c1556287a9b0d79816ea3470fa70
luislimait <pls.luis@gmail.com>
2022-09-18 22:39:04 -0300

    Tipos de dados

---
+++ tipos.php
@@ -0,0 +1,12 @@
+<?php
+
+$idade = 31;
+$salario = 1000.30;
+$divisao = 10 /3;
+$nome = "Luis";
+$sabeProgramar = true;
+
+echo gettype($divisao);
+
+/*O PHP é uma linguagem dinamicamente tipada, ou seja, consegue
+descobrir o tipo do dado baseado no seu valor :) */
