57ff975b0edd995b4f441431ae3496dbd0e0fb5d
luislimait <pls.luis@gmail.com>
2022-09-19 18:25:49 -0300

    Caracteres especiais

--- strings.php
+++ strings.php
@@ -4,6 +4,10 @@ $idade = 31;
 $cidade = "São Caetano";
 $estado = "São Paulo";
 
-echo "Olá mundo, minha idade é $idade anos";
+echo "Olá mundo, minha idade é $idade anos.\r\n";
+echo "Sou de $cidade, $estado." . PHP_EOL;
+echo 'PHL_EOL tbm pula linha';
 
-echo " Sou de $cidade, $estado";
+// \t = TAB
+// \n = Quebra de linha
+// Para saber mais: https://www.php.net/manual/pt_BR/language.types.string.php
