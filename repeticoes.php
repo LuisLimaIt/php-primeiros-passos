20ee908913efdd08c18d961ec6a0a0fd30c0e5ac
luislimait <pls.luis@gmail.com>
2022-09-21 12:16:18 -0300

    Repetições

--- repeticoes.php
+++ repeticoes.php
@@ -2,10 +2,10 @@
 
 $contador = 0;
 
-// while ($contador <= 15) {
-//     echo "#$contador" . PHP_EOL;
-//     $contador++;
-// }
+while ($contador <= 15) {
+    echo "#$contador" . PHP_EOL;
+    $contador++;
+}
 
 for ($i=0; $i <= 15; $i++) { 
     echo "#$i" . PHP_EOL;
