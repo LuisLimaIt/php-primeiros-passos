e7ab8298c48de31846bae0d8a95d46a5db338bee
luislimait <pls.luis@gmail.com>
2022-09-21 11:30:31 -0300

    Condicionais

--- decisoes.php
+++ decisoes.php
@@ -1,14 +1,20 @@
 <?php
 
-$idade = 17;
-$nome  = "Paulo";
+$idade = 18;
+$temAcompanhante  = true;
 
-echo "Você só pode entrar se tiver a partir de 18 anos" . PHP_EOL;
+echo "Você só pode entrar se tiver a partir de 18 anos e estiver acompanhado(a) \n" . PHP_EOL;
 
-if ($idade >= 18 && $nome === "Paulo") {
-    echo "Olá $nome, você é um xófem de $idade anos." . PHP_EOL;;
+if ($idade >= 18 && $temAcompanhante) {
+    echo "Você tem $idade anos e está acompanhado(a)." . PHP_EOL;;
     echo "Pode entrar!";
+} else if ($idade >= 18 && !$temAcompanhante) {
+    echo "Você tem $idade anos, mas não está acompanhado(a)." . PHP_EOL;
+    echo "Volte para casa!";
+} else if ($idade < 18 && $temAcompanhante) {
+    echo "Você está acompanhado(a), mas tem apenas $idade anos." . PHP_EOL;
+    echo "Volte para casa!";
 } else {
-    echo "Você é menor de idade ou seu nome não está na lista." . PHP_EOL;
+    echo "Você não está acompanhado(a) e tem apenas $idade anos." . PHP_EOL;
     echo "Volte para casa!";
 }
