65b3a53073f409652d70f4ac1d2c0775caebb36d
luislimait <pls.luis@gmail.com>
2022-09-23 19:48:50 -0300

    Funções

--- Avancando/banco.php
+++ Avancando/banco.php
@@ -1,8 +1,17 @@
 <?php
 
+function sacar($contaCorrente, $valorDoSaque) {
+    if($valorDoSaque > $contaCorrente['saldo']) {
+        exibeMensagem("[" . $contaCorrente['titular'] . "] = SALDO INSUFICIENTE");
+    } else {
+        $contaCorrente['saldo'] -=  $valorDoSaque;
+    }
+    return $contaCorrente;
+};
+
 function exibeMensagem($mensagem) {
     echo $mensagem . PHP_EOL;
-}
+};
 
 $contasCorrentes = [
     '444.222.555-88' =>  ['titular' => 'Luis', 'saldo' => 6180],  
@@ -10,20 +19,10 @@ $contasCorrentes = [
     '999.333.555-66' => ['titular' => 'Alan', 'saldo' => 14575]
 ];
 
-$valorSaque = 8200;
+$contasCorrentes['444.222.555-88'] = sacar($contasCorrentes['444.222.555-88'], 6180);
+$contasCorrentes['999.444.777-33'] = sacar($contasCorrentes['999.444.777-33'], 9180);
 
-if($valorSaque > $contasCorrentes['444.222.555-88']['saldo']) {
-    exibeMensagem("[" . $contasCorrentes['444.222.555-88']['titular'] . "] = SALDO INSUFICIENTE");
-} else {
-    $contasCorrentes['444.222.555-88']['saldo'] -=  $valorSaque;
-}
-
-if($valorSaque > $contasCorrentes['999.444.777-33']['saldo']) {
-    exibeMensagem("[" . $contasCorrentes['999.444.777-33']['titular'] . "] = SALDO INSUFICIENTE");
-} else {
-    $contasCorrentes['999.444.777-33']['saldo'] -= $valorSaque;
-}
 
 foreach ($contasCorrentes as $cpf => $conta) {
     exibeMensagem($conta['titular'] . " = CPF: $cpf Saldo: " . $conta['saldo']);
-}
+};
