6426eee1869016a68cae0b6a2feb0e63a2efeff6
luislimait <pls.luis@gmail.com>
2022-09-23 20:27:42 -0300

    Arquivos separados

--- Avancando/banco.php
+++ Avancando/banco.php
@@ -1,29 +1,6 @@
 <?php
 
-function sacar(array $contaCorrente, float $valorDoSaque): array 
-{
-    if($valorDoSaque > $contaCorrente['saldo']) {
-        exibeMensagem("[{$contaCorrente['titular']}] = SALDO INSUFICIENTE");
-    } else {
-        $contaCorrente['saldo'] -=  $valorDoSaque;
-    }
-    return $contaCorrente;
-};
-
-function depositar(array $contaCorrente, float $valorDoDeposito): array 
-{
-    if ($valorDoDeposito > 0) {
-        $contaCorrente['saldo'] +=  $valorDoDeposito;
-    } else {
-        echo "O valor do depósito não pode ser negativo!" . PHP_EOL;
-    }
-    return $contaCorrente;
-};
-
-function exibeMensagem(string $mensagem) 
-{
-    echo $mensagem . PHP_EOL;
-};
+require_once 'funcoes.php';
 
 $contasCorrentes = [
     '444.222.555-88' =>  ['titular' => 'Luis', 'saldo' => 6180],  
---
+++ Avancando/funcoes.php
@@ -0,0 +1,26 @@
+<?php
+
+function sacar(array $contaCorrente, float $valorDoSaque): array 
+{
+    if($valorDoSaque > $contaCorrente['saldo']) {
+        exibeMensagem("[{$contaCorrente['titular']}] = SALDO INSUFICIENTE");
+    } else {
+        $contaCorrente['saldo'] -=  $valorDoSaque;
+    }
+    return $contaCorrente;
+};
+
+function depositar(array $contaCorrente, float $valorDoDeposito): array 
+{
+    if ($valorDoDeposito > 0) {
+        $contaCorrente['saldo'] +=  $valorDoDeposito;
+    } else {
+        echo "O valor do depósito não pode ser negativo!" . PHP_EOL;
+    }
+    return $contaCorrente;
+};
+
+function exibeMensagem(string $mensagem) 
+{
+    echo $mensagem . PHP_EOL;
+};
