b2f5e9cb7d7816b8ae1cd057d17c46f9a0ed57be
luislimait <pls.luis@gmail.com>
2022-09-23 19:22:52 -0300

    Subrotinas

--- Avancando/banco.php
+++ Avancando/banco.php
@@ -1,25 +1,29 @@
 <?php
 
+function exibeMensagem($mensagem) {
+    echo $mensagem . PHP_EOL;
+}
+
 $contasCorrentes = [
     '444.222.555-88' =>  ['titular' => 'Luis', 'saldo' => 6180],  
     '999.444.777-33' => ['titular' => 'Carlos', 'saldo' => 6730], 
     '999.333.555-66' => ['titular' => 'Alan', 'saldo' => 14575]
 ];
 
-$valorSaque = 6200;
+$valorSaque = 8200;
 
 if($valorSaque > $contasCorrentes['444.222.555-88']['saldo']) {
-    echo $contasCorrentes['444.222.555-88']['titular'] . " Não pode realizar o saque - Saldo insuficiente! \n" .PHP_EOL;
+    exibeMensagem("[" . $contasCorrentes['444.222.555-88']['titular'] . "] = SALDO INSUFICIENTE");
 } else {
     $contasCorrentes['444.222.555-88']['saldo'] -=  $valorSaque;
 }
 
 if($valorSaque > $contasCorrentes['999.444.777-33']['saldo']) {
-    echo $contasCorrentes['999.444.777-33']['titular'] . " Não pode realizar o saque - Saldo insuficiente! \n" .PHP_EOL;
+    exibeMensagem("[" . $contasCorrentes['999.444.777-33']['titular'] . "] = SALDO INSUFICIENTE");
 } else {
     $contasCorrentes['999.444.777-33']['saldo'] -= $valorSaque;
 }
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    echo $conta['titular'] . " = CPF: $cpf Saldo: " . $conta['saldo'] . PHP_EOL;
+    exibeMensagem($conta['titular'] . " = CPF: $cpf Saldo: " . $conta['saldo']);
 }
